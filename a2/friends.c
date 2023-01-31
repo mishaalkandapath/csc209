b724675229b0dba7e04cab9ee81d1e140419d668
kandapat <markus@markus.com>
2023-01-31 00:07:32 -0500

    Update files

---
+++ a2/friends.c
@@ -0,0 +1,119 @@
+#include "friends.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * Create a new user with the given name.  Insert it at the tail of the list
+ * of users whose head is pointed to by *user_ptr_add.
+ *
+ * Return:
+ *   - 0 if successful
+ *   - 1 if a user by this name already exists in this list
+ *   - 2 if the given name cannot fit in the 'name' array
+ *       (don't forget about the null terminator)
+ */
+int create_user(const char *name, User **user_ptr_add) {
+    return -1;
+}
+
+
+/*
+ * Return a pointer to the user with this name in
+ * the list starting with head. Return NULL if no such user exists.
+ *
+ * NOTE: You'll likely need to cast a (const User *) to a (User *)
+ * to satisfy the prototype without warnings.
+ */
+User *find_user(const char *name, const User *head) {
+    return NULL;
+}
+
+
+/*
+ * Print the usernames of all users in the list starting at curr.
+ * Names should be printed to standard output, one per line.
+ */
+void list_users(const User *curr) {
+}
+
+
+/*
+ * Change the filename for the profile pic of the given user.
+ *
+ * Return:
+ *   - 0 on success.
+ *   - 1 if the file does not exist or cannot be opened.
+ *   - 2 if the filename is too long.
+ */
+int update_pic(User *user, const char *filename) {
+    return -1;
+}
+
+
+/*
+ * Make two users friends with each other.  This is symmetric - a pointer to
+ * each user must be stored in the 'friends' array of the other.
+ *
+ * New friends must be added in the first empty spot in the 'friends' array.
+ *
+ * Return:
+ *   - 0 on success.
+ *   - 1 if the two users are already friends.
+ *   - 2 if the users are not already friends, but at least one already has
+ *     MAX_FRIENDS friends.
+ *   - 3 if the same user is passed in twice.
+ *   - 4 if at least one user does not exist.
+ *
+ * Do not modify either user if the result is a failure.
+ * NOTE: If multiple errors apply, return the *largest* error code that applies.
+ */
+int make_friends(const char *name1, const char *name2, User *head) {
+    return -1;
+}
+
+
+/*
+ * Print a user profile.
+ * For an example of the required output format, see the example output
+ * linked from the handout.
+ * Return:
+ *   - 0 on success.
+ *   - 1 if the user is NULL.
+ */
+int print_user(const User *user) {
+    return -1;
+}
+
+
+/*
+ * Make a new post from 'author' to the 'target' user,
+ * containing the given contents, IF the users are friends.
+ *
+ * Insert the new post at the *front* of the user's list of posts.
+ *
+ * 'contents' is a pointer to heap-allocated memory - you do not need
+ * to allocate more memory to store the contents of the post.
+ *
+ * Return:
+ *   - 0 on success
+ *   - 1 if users exist but are not friends
+ *   - 2 if either User pointer is NULL
+ */
+int make_post(const User *author, User *target, char *contents) {
+    return -1;
+}
+
+
+/*
+ * From the list pointed to by *user_ptr_del, delete the user
+ * with the given name.
+ * Remove the deleted user from any lists of friends.
+ *
+ * Return:
+ *   - 0 on success.
+ *   - 1 if a user with this name does not exist.
+ */
+int delete_user(const char *name, User **user_ptr_del) {
+    return -1;
+}
