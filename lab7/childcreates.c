87467d1d7ff0ae2b0e8c4a2a96eb7312f1048142
Kandapath Mishaal <kandapat@teach.cs.toronto.edu>
2023-03-03 17:31:56 -0500

    lab 7 remived exiting solution'

--- lab7/childcreates.c
+++ lab7/childcreates.c
@@ -10,17 +10,20 @@ int main(int argc, char **argv) {
     }
 
     int iterations = strtol(argv[1], NULL, 10);
-
+    int done = 0;
     for (int i = 0; i < iterations; i++) {
-        int n = fork();
-        if (n < 0) {
-            perror("fork");
-            exit(1);
+        if (done == 0){
+            done++;
+            int n = fork();
+            if (n < 0) {
+                perror("fork");
+                exit(1);
+            }
+            if (n == 0){
+                done = 0;
+            }
         }
-        printf("ppid = %d, pid = %d, i = %d\n", getppid(), getpid(), i);
-        if (n > 0){
-            break;
-        }
+         printf("ppid = %d, pid = %d, i = %d\n", getppid(), getpid(), i);
     }
     sleep(1);
     return 0;
--- lab7/parentcreates.c
+++ lab7/parentcreates.c
@@ -11,16 +11,19 @@ int main(int argc, char **argv) {
 
     int iterations = strtol(argv[1], NULL, 10);
 
+    int child = 0;
     for (int i = 0; i < iterations; i++) {
-        int n = fork();
-        if (n < 0) {
-            perror("fork");
-            exit(1);
+        if (!child){
+            int n = fork();
+            if (n < 0) {
+                perror("fork");
+                exit(1);
+            }
+            if (n == 0){
+                child = 1;
+            }
         }
         printf("ppid = %d, pid = %d, i = %d\n", getppid(), getpid(), i);
-        if (n == 0){
-            exit(0);
-        }
     }
 
     return 0;
