3fc31483a15b8e2d9b31b46489237c50f59a81d8
Kandapath Mishaal <kandapat@teach.cs.toronto.edu>
2023-03-08 15:13:10 -0500

    part 1

--- a3/parse.c
+++ a3/parse.c
@@ -5,6 +5,21 @@
 #include "pmake.h"
 
 
+void remove_character_return(char *line){
+    int len = strlen(line);
+    for (int i =0; i<len; i++){
+        if (line[i] == 13){
+            for (int j = i; j < len - 1; j++) {
+                line[j] = line[j + 1];
+            }
+            len--;
+            i--;
+        }
+    }
+
+    line[len] = '\0';
+}
+
 Rule * get_rule(Rule *head, char * target_name){
     Rule *curr_rule = head;
     while (curr_rule != NULL){
@@ -17,7 +32,7 @@ Rule * get_rule(Rule *head, char * target_name){
     return NULL;
 }
 
-Rule * link_dependencies(char * targets, Dependency **curr_dependency, Rule *head, Rule *curr_node){
+Rule * link_dependencies(char * targets, Dependency **curr_dependency, Rule *head, Rule *curr_node, Rule** tail_node){
     
     //set the dependency links, create rules if they dont exist:
     Rule *dep_rule = get_rule(head, targets);
@@ -28,7 +43,11 @@ Rule * link_dependencies(char * targets, Dependency **curr_dependency, Rule *hea
         strncpy(dep_rule -> target, targets, strlen(targets));
         (dep_rule -> target)[strlen(targets)] = '\0';
         dep_rule -> next_rule = NULL;
-        curr_node -> next_rule = dep_rule;
+        (*tail_node) -> next_rule = dep_rule;
+        (*tail_node) = dep_rule;
+        (*tail_node) -> next_rule = NULL;
+        (*tail_node) -> dependencies = NULL;
+        (*tail_node) -> actions = NULL;
         created = 1;
     }
 
@@ -83,6 +102,7 @@ Rule *parse_file(FILE *fp) {
     while(fgets(line, MAXLINE, fp) != NULL){ //go through the file line by line
         int type = line_type(line); //get the type of the line
         line[strlen(line) - 1] = '\0'; //remove the newline character
+        remove_character_return(line);
 
         if (type == 0){// it is a rule line:
             char *targets; //where all the tokens will be filled
@@ -96,18 +116,22 @@ Rule *parse_file(FILE *fp) {
                 head = malloc(sizeof(Rule));
                 curr_rule = head;
                 curr_tail = head;
+                head -> next_rule = NULL;
+                head ->dependencies = NULL;
+                head -> actions = NULL;
             }else{
                 //if a rule of this name does not exist, create it
                 Rule *new_rule = get_rule(head, targets);
                 if (new_rule == NULL){
                     new_rule = malloc(sizeof(Rule));
+                    curr_tail -> next_rule = new_rule;
+                    curr_tail = new_rule;
+                    curr_tail-> actions = NULL;
+                    curr_tail -> dependencies = NULL;
+                    curr_tail -> next_rule = NULL;
                 }
-                curr_rule -> next_rule = new_rule;
-                curr_rule = new_rule; //set it to be the most upd to date node.
+                curr_rule = new_rule; //set it to be the most upd to date node.   
             }
-            curr_rule ->dependencies = NULL;
-            curr_rule -> actions = NULL;
-            curr_rule -> next_rule = NULL;
 
             //copy in the name of the target
             curr_rule -> target = malloc(sizeof(char) * (strlen(targets) + 1));
@@ -118,27 +142,20 @@ Rule *parse_file(FILE *fp) {
             while(targets != NULL){
                 targets = strtok(NULL, " ");
                 if (targets != NULL && (((int) targets[0]) != 13)){
-                    // if (strchr(targets, '\n') != NULL){
-                    //     targets[strlen(targets - 1)] = '\0';
-                    // }
-                    Rule * maybe_node = link_dependencies(targets, &curr_dependency, head, curr_rule);
-                    if (maybe_node != NULL){
-                        curr_rule -> next_rule = maybe_node;
-                        curr_rule = maybe_node;
-
-                        curr_rule -> next_rule = NULL;
-                        curr_rule ->dependencies = NULL;
-                        curr_rule -> actions = NULL;
-                    }
+                    link_dependencies(targets, &curr_dependency, head, curr_rule, &curr_tail);
                 }
             }
 
             //reset dependencies and actions
             curr_dependency = NULL;
             curr_action = NULL; 
+
+            // printf("at the end %s\n", curr_rule -> target);
+            // printf("at the tail %s\n", curr_tail -> target);
+            // printf("at the head %s\n", head -> target);
         }else if (type == 1){
             //create a new actions
-            printf("%s\n", line);
+            // printf("%s\n", line);
             Action * new_action = malloc(sizeof(Action));
 
             if (curr_action != NULL){
@@ -210,8 +227,7 @@ void print_rules(Rule *rules){
     Rule *cur = rules;
 
     while (cur != NULL) {
-        printf("%s\n", cur->target);
-        printf("%s\n", cur -> dependencies -> rule -> target);
+        // printf("%s\n", cur->target);
         if (cur->dependencies || cur->actions) {
             // Print target
             printf("%s: ", cur->target);
