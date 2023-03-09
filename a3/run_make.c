43ed4f2e21da06d858791a1f577b57c34d5c330b
Kandapath Mishaal <kandapat@teach.cs.toronto.edu>
2023-03-09 18:24:12 -0500

    part 3

--- a3/run_make.c
+++ a3/run_make.c
@@ -33,11 +33,10 @@ void run_actions(Action* action){
       }
       action = action -> next_act;
     }
-    }
+  }
 }
 
-  int evaluate_rules(Rule* rule, Rule* rules){
-    // printf("target here %s p1: %p p2: %p\n", rule -> target, rule->dependencies, rule->actions);
+  int evaluate_rules(Rule* rule, int pflag){
 
     if (rule -> dependencies == NULL && rule -> actions == NULL){
       //nothing to do here, return some random shet
@@ -48,29 +47,73 @@ void run_actions(Action* action){
       // this rule has dependencies
       Dependency *curr_dep = rule -> dependencies;
       int update_reqd = 0;
-      while (curr_dep != NULL){
-
-        if (evaluate_rules(curr_dep -> rule, rules) == 1){
-            update_reqd = 1;
-          }
+      int forked = 1;
+      int children = 0;
+      while (curr_dep){
+        children++;
         curr_dep = curr_dep -> next_dep;
       }
+      //resetting
+      curr_dep = rule -> dependencies;
+      int pipfd[children][2];
 
-      if (update_reqd){
+      children = 0;
+      while (curr_dep){
+        if (pflag && forked){
+          pipe(pipfd[children]); //pass in the right file directories
+          close(pipfd[children][1]); //they dont need to write. 
+          //create child if necessary 
+          forked = pflag ? fork() : 0;
+          children += pflag ? 1 : 0;
+        }
+    
+        if (!forked || !pflag){
+          update_reqd = evaluate_rules(curr_dep -> rule, pflag);
+          if (!forked){
+            close(pipfd[children - 1][0]);// children are not reading
+            //close before forks reading ends
+            for (int j = 0; j < children -1 ; j++){
+              close(pipfd[j][0]);
+            }
+            write(pipfd[children - 1][1], &update_reqd, sizeof(int));
+            close(pipfd[children - 1][1]);
+            break; //coz one dependency per child policy
+          }
+        }
+        if (!pflag || forked) {
+          curr_dep = curr_dep -> next_dep;
+        }
+      }
+
+      if (children && pflag && forked){
+        int temp_upg = 0;
+        for (int i = 0; i < children; i++){
+          int status;
+          wait(&status); // wait for each child.
+          read(pipfd[i][0], &temp_upg, sizeof(int));
+          close(pipfd[i][0]);
+          update_reqd += temp_upg;
+        }
+      }
+
+      update_reqd = forked ? update_reqd : -1; //if this is a child return -1;
+      // printf("upgrade status for %s %d %d\n",rule -> target, update_reqd, forked);
+
+      if (update_reqd >= 1){//coz it can be -1 
         //evaluate all the actions, if any
         Action *curr_action = rule -> actions;
         if (curr_action != NULL){
           run_actions(curr_action);
         }
         return 1;
-      }else{
+      }else if (update_reqd != -1){
         //check the dependency creation times;
         //if the current target does not exist, or any of its dependencies do not exist, or modification times of its dependencies more than its modification, then run actions
 
         //check if the target file exists:
         struct stat stat_info;
         stat(rule -> target, &stat_info);
-        if (errno == 2){
+        if (errno != 0){
           //no such file
           Action *curr_action = rule -> actions;
           if (curr_action != NULL){
@@ -83,7 +126,7 @@ void run_actions(Action* action){
           long nanosecs = (stat_time.tv_nsec);
           //check the dependencies:
           curr_dep = rule -> dependencies;
-          while(curr_dep != NULL){
+          while(curr_dep){
             stat(curr_dep -> rule -> target, &stat_info);
             if (errno != 0){
               update_reqd = 1;
@@ -91,7 +134,7 @@ void run_actions(Action* action){
               //modification time of the dependency file
               long dep_secs = ((stat_info.st_mtim).tv_sec);
               long dep_nsecs = ((stat_info.st_mtim).tv_nsec);
-              if (dep_secs >= seconds || (dep_secs == seconds && dep_nsecs >= nanosecs)){
+              if (dep_secs > seconds || (dep_secs == seconds && dep_nsecs >= nanosecs)){
                 update_reqd = 1;
               }
             }
@@ -131,13 +174,14 @@ void run_make(char *target, Rule *rules, int pflag) {
   }
 
   Rule *rel_rule = get_rule(rules, target);
+  
   if (rel_rule == NULL){
     //no such target exists
     fprintf(stderr, "No such target (%s) found\n", target);
     exit(1);
   }
 
-  evaluate_rules(rel_rule, rules);
+  evaluate_rules(rel_rule, pflag);
 
 }
 
